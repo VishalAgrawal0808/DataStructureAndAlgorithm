0fde2a77354c1425cce6a50895965fe4d027777e
VishalAgrawal0808 <vishal.agrawal0808@gmail.com>
2022-02-13 09:27:51 +0530

    First Commit

---
+++ src/com/DataStructure/BinarySearch/BinarySearchAlgorithm.java
@@ -0,0 +1,25 @@
+package com.DataStructure.BinarySearch;
+
+public class BinarySearchAlgorithm {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int ele =  4;
+        System.out.println(BinarySearchAlgorithmCode(arr, ele));
+    }
+
+    static boolean BinarySearchAlgorithmCode(int[] a, int ele){
+        int start = 0, end = a.length-1;
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(ele == a[mid])
+                return true;
+            else if(ele>a[mid]){
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        return false;
+    }
+}
