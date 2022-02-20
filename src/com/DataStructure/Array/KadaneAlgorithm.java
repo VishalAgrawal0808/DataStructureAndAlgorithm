8a10fba697d1e3c131a8c5c4c6a375be6c876049
VishalAgrawal0808 <vishal.agrawal0808@gmail.com>
2022-02-20 22:05:22 +0530

    adding new java file

---
+++ src/com/DataStructure/Array/KadaneAlgorithm.java
@@ -0,0 +1,14 @@
+package com.DataStructure.Array;
+
+public class KadaneAlgorithm {
+    public static void main(String[] args) {
+        int[] a = {-5, 4, 6, -3, 4, -1};
+        KadaneAlgorithmCode(a);
+    }
+    static void KadaneAlgorithmCode(int[] a){
+        for(int i=0;i<a.length;i++){
+            int[] b;
+
+        }
+    }
+}
